8ffaca5ec139636b57d6e22213a80e0512c3c4f6
dmytropoliashenko <polyashenko1@gmail.com>
2023-10-31 11:46:04 +0200

    first commit

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -1,43 +1,91 @@
 package org.example;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.TimeZone;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/time")
 public class TimeServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // Встановлюємо MIME тип відповіді на текст/HTML
-        response.setContentType("text/html");
 
-        // Отримуємо значення параметра "timezone" з запиту
-        String timezoneParam = request.getParameter("timezone");
-        TimeZone timeZone = TimeZone.getTimeZone("UTC"); // За замовчуванням UTC
+    public static final String DEFAULT_TIMEZONE = "UTC";
+    public static final String PARAM_TIMEZONE = "timezone";
+    public static final String CONTENT_TYPE_HTML = "text/html";
+    public static final int MILLISECONDS_IN_HOUR = 3600000; // 60 * 60 * 1000 :)
 
-        // Якщо параметр "timezone" був переданий, то встановлюємо відповідний часовий пояс
-        if (timezoneParam != null && !timezoneParam.isEmpty()) {
-            timeZone = TimeZone.getTimeZone(timezoneParam);
+    private transient TemplateEngine engine;
+
+    /**
+     * Initializes the servlet by setting up the Thymeleaf template engine with custom settings.
+     */
+    @Override
+    public void init() {
+        engine = new TemplateEngine();
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setPrefix("/Users/dmytropoliashenko/work/Java/GoIt/java_developer_module_12/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(engine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+        engine.addTemplateResolver(resolver);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType(CONTENT_TYPE_HTML);
+        TimeZone timezone;
+        String timezoneParam = request.getParameter(PARAM_TIMEZONE);
+        if (timezoneParam != null && !timezoneParam.trim().isEmpty()) {
+            ZoneId zoneId = ZoneId.of(timezoneParam);
+            timezone = TimeZone.getTimeZone(zoneId);
+            saveTimezoneToCookie(response, timezone);
+        } else {
+            timezone = getTimezoneFromCookie(request);
         }
 
-        // Отримуємо поточний час у форматі "yyyy-MM-dd HH:mm:ss"
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date currentTime = new Date();
-        String currentTimeStr = sdf.format(currentTime);
 
-        // Відправляємо HTML сторінку у відповідь
-        response.getWriter().println("<html>");
-        response.getWriter().println("<head><title>Current time in UTC</title></head>");
-        response.getWriter().println("<body>");
-        response.getWriter().println("<h1>Current time in UTC</h1>");
-        response.getWriter().println("<p>Time: " + currentTimeStr + " UTC</p>");
-        response.getWriter().println("</body>");
-        response.getWriter().println("</html>");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+        dateFormat.setTimeZone(timezone);
+        String currentTime = dateFormat.format(new Date());
+
+
+        Context context = new Context();
+        context.setVariable("currentTime", currentTime);
+        context.setVariable("availableTimezones", ZoneId.getAvailableZoneIds());
+
+
+        engine.process("time", context, response.getWriter());
+        response.getWriter().close();
+    }
+
+    // Метод для отримання часового поясу із Cookie, за замовчуванням - UTC
+    private TimeZone getTimezoneFromCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("lastTimezone".equals(cookie.getName())) {
+                    String timezoneID = cookie.getValue();
+                    return TimeZone.getTimeZone(timezoneID);
+                }
+            }
+        }
+        // Якщо Cookie "lastTimezone" не знайдено, повертаємо часовий пояс UTC за замовчуванням
+        return TimeZone.getTimeZone(DEFAULT_TIMEZONE);
+    }
+
+    // Метод для збереження часового поясу в Cookie
+    private void saveTimezoneToCookie(HttpServletResponse response, TimeZone timezone) {
+        Cookie timezoneCookie = new Cookie("lastTimezone", timezone.getID());
+        response.addCookie(timezoneCookie);
     }
 }
